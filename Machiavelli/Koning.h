06e90465a681108357bad21e64db848994187bca
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2019-03-28 12:37:22 +0100

    King done

--- Machiavelli/Koning.h
+++ Machiavelli/Koning.h
@@ -11,7 +11,7 @@ public:
 	Koning(Game& game);
 	~Koning();
 
-	void onEnter() override {};
+	void onEnter() override;
 	void onLeave() override {};
 	bool act(ClientInfo& clientInfo, std::string cmd) override;
 
